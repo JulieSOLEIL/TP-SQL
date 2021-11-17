f48be85db7882e9ac88c5c3fd6bbc1cc3a816cb5
SUN Julie <90616280+JulieSOLEIL@users.noreply.github.com>
2021-11-17 10:22:50 +0100

    Create Fruit.sql

---
+++ Fruit.sql
@@ -0,0 +1,14 @@
+Explication de la base : 
+Plusieurs tables sont liées entre elles par des clefs étrangères. 
+Créer des requêtes avec jointure avec WHERE égalité des attributs ou INNER JOIN … 
+ON égalité des attributs 
+
+1. Rechercher et classer les fruits français, puis les fruits d'un genre donné 
+2. Compter le nombre de fruits d'un genre (ex : figue) 
+3. Rechercher les fournisseurs d'un genre (ex : figue) 
+4. Quels sont les fournisseurs de fruits français? 
+5. Quels sont les fruits les moins chers au kilo? 
+6. Afficher le prix moyen de vente des fruits par genre 
+7. Lister pour chaque fournisseur ses genres de fruits avec le nombre de variétés de 
+fruits par genre. 
+8. Augmenter le prix des figues de 0,5
