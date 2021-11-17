bbcd75e2dc3a5aab95512616469b788ad508c6d4
SUN Julie <90616280+JulieSOLEIL@users.noreply.github.com>
2021-11-17 15:53:40 +0100

    Create films.sql

---
+++ films.sql
@@ -0,0 +1,28 @@
+Réaliser les requêtes suivantes : 
+1. Nom et année de naissance des artistes nés avant 1950 
+2. Titre de tous les drames 
+3. Quels rôles a joué Bruce Willis (sans doublon) 
+4. Qui est le réalisateur de Memento 
+5. Quelles sont les notes obtenues par le film Fargo 
+6. Qui a joué le rôle de Chewbacca? 
+7. Dans quels films Bruce Willis a-t-il joué le rôle de John McClane ? 
+8. Nom des acteurs de 'Sueurs froides' avec leur rôle
+9. Quelles sont les films notés par l'internaute Prénom10000 Nom10000 
+10. Films dont le réalisateur est Tim Burton, et l’un des acteurs Jonnhy Depp. 
+11. Titre des films dans lesquels a joué Woody Allen. Donner aussi le rôle. 
+12. Quel metteur en scène a tourné́ dans ses propres films ? Donner le nom, le rôle et le 
+titre des films. 
+13. Titre des films de Quentin Tarantino dans lesquels il n’a pas joué 
+14. Quel metteur en scène a tourné en tant qu’acteur ? Donner le nom, le rôle et le titre 
+des films dans lesquels cet artiste a joué. 
+15. Donnez les films de Hitchcock sans James Stewart 
+16. Dans quels films le réalisateur a-t-il le même prénom que l’un des interprètes ? 
+(titre, nom du réalisateur, nom de l’interprète). 
+Le réalisateur et l’interprète ne doivent pas être la même personne. 
+17. Les films sans rôle 
+18. Quelles sont les films non notés par l'internaute Prénom1 Nom1 
+19. Quels acteurs n’ont jamais réalisé de film ? 
+20. Quelle est la moyenne des notes de Memento 
+21. id, Nom et prénom des réalisateurs, et nombre de films qu’ils ont tournés. 
+22. Nom et prénom des réalisateurs qui ont tourné au moins deux films. 
+23. Quels films ont une moyenne des notes supérieure à 7
